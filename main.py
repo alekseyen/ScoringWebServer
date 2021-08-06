a451f0e219c04ddf64fbc98246f93ca0ac73221f
aleksejpodkidysev <podkidyshev.as@phystech.edu>
2021-08-07 00:12:32 +0300

    mlflow fixing

--- main.py
+++ main.py
@@ -49,6 +49,7 @@ app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 if not os.path.exists(UPLOAD_FOLDER):
     os.mkdir(UPLOAD_FOLDER)
 
+
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
